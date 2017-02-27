474fb8bd099546b3bfc4949613488f316f7321cf
Mike Gasparelli <magasparel@gmail.com>
2017-02-27 12:34:43 -0500

    Add SQL Server cheat sheet

---
+++ SqlServer.sql
@@ -0,0 +1,46 @@
+--Create a new table
+CREATE TABLE Person (
+   ID int NOT NULL IDENTITY,
+   Name nvarchar(max),
+   Salary decimal(18, 2) NOT NULL,
+   Age int NOT NULL,
+   CONSTRAINT PK_dbo.Person PRIMARY KEY (ID)
+)
+
+--Add a primary key to an existing table
+ALTER TABLE Tacos
+ADD PRIMARY KEY (ID)
+
+--Add a column to a table
+ALTER TABLE Person
+ADD BirthDate DATETIME NOT NULL
+
+--Change column properties
+ALTER TABLE Person
+ALTER COLUMN NAME nvarchar(max) NOT NULL
+
+--Remove a column from a table
+ALTER TABLE Person
+DROP COLUMN Age
+
+--Rename a column
+--Qualify the first parameter with the table name, not the second
+EXEC sp_rename 'Person.Salary', 'Wage', 'COLUMN'; 
+
+--Rename a table
+EXEC sp_rename 'Persons', 'Person';
+
+--Allow insert statements to specify IDs
+SET IDENTITY_INSERT Table1 ON;
+
+--Insert rows from one table into another
+INSERT INTO Table1
+SELECT name, GETDATE(), '123 Street Ave.', status FROM Table2;
+
+--Insert rows from one table into another *with IDENTITY_INSERT*
+INSERT INTO Table1 (id, name, createdtimestamp, updatedtimestamp)
+SELECT id, name, createdtimestamp, updatedtimestamp FROM Table2;
+
+--Run a query on all tables in the database
+sp_MSforeachtable 'alter table ? check constraint all'
+
